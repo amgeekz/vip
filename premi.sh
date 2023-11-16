d9dff447c7da4f8ab05ac5ef96cbfffea378fd5a
amgeekz <79773348+amgeekz@users.noreply.github.com>
2023-11-17 05:54:21 +0700

    Add files via upload

--- premi.sh
+++ premi.sh
@@ -128,10 +128,10 @@ Error="${RED}Expired${NC}"
 if [[ "$certifacate" -le "0" ]]; then
 sts="${Error}"
 echo -e " $BLUE╭──────────────────────────────────────────────────────────╮${NC}"
-echo -e " $BLUE│$NC$RED    IP address not authorized by admin$NC"
-echo -e " $BLUE│$NC$RED    Please contact admin to rent this script$NC"
-echo -e " $BLUE│$NC$r • $NC$WHITE Whatsapp$NC  ${WHITE}:$GREEN 6285649455626$NC"
-echo -e " $BLUE│$NC$r • $NC$WHITE Telegram  :$NC $GREEN amgeekzssh$NC "
+echo -e " $BLUE│$NC$RED    IP address not authorized by admin $NC"
+echo -e " $BLUE│$NC$RED    Please contact admin to rent this script $NC"
+echo -e " $BLUE│$NC$r • $NC$WHITE Whatsapp :$NC $GREEN 6285649455626$NC"
+echo -e " $BLUE│$NC$r • $NC$WHITE Telegram  :$NC $GREEN amgeekzssh$NC"
 echo -e " $BLUE╰──────────────────────────────────────────────────────────╯${NC}"
 sleep 3
 exit 1
